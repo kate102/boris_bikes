8071ccf71ab2bc47ff132001a67cbab2df9b82f5
Student <student@Admins-MacBook-Pro-4.local>
2019-02-07 14:07:44 +0000

    catching up

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,25 @@
 require './lib/bike'
-
-class DockingStation
-
-  def release_bike
-    Bike.new
-  end
-
-end
+	
+	class DockingStation
+	  attr_reader :bike
+	
+	  def initialize
+	    # @bikes = []
+	  end
+	
+	  def release_bike
+	    # bikes.last.delete
+	    Bike.new
+	    # remove bike from array bikes.delete_if ....
+	  end
+	
+	  def dock(bike)
+	    @bike = bike
+	  #   bikes.push(bike)
+	  end
+	
+	  def bike
+	    @bike
+	  end
+	
+	end
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,5 +1,13 @@
 require 'docking_station'
-
-describe DockingStation do
-  it { is_expected.to respond_to :release_bike}
-end
+	
+	
+	describe DockingStation do
+	
+	  it { is_expected.to respond_to :release_bike }
+	  # it { is_expected.to respond_to(:dock).with(1).argument }
+	  it 'docks something' do
+	    bike = Bike.new
+	    expect(subject.dock(bike)).to eq bike
+	  end
+	  it { is_expected.to respond_to(:bike) }
+	end
